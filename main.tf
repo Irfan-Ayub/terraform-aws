f57775119258acb30fba1bcbf0432449049d3e65
Muhammad Irfan Ayub <irfanayub995@gmail.com>
2020-09-01 17:37:05 +0500

    Uploaded Files
    Demo Implementation of an infrastructure using Terraform AWS

---
+++ main.tf
@@ -0,0 +1,154 @@
+variable "subnet-prefix" {
+  description = "Subnet Prefix for CIDR Block"
+}
+
+#The Access keys are not working need to change then accordingly.
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "AKIAJ36F5Y4FLXOPB2AA"
+  secret_key = "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"
+}
+
+# 1. Create VPC
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "production"
+  }
+}
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+}
+
+# 3. Create Custom Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# 4. Create a Subnet
+
+resource "aws_subnet" "subnet-1" {
+  cidr_block = var.subnet-prefix[0].cidr_block
+  vpc_id = aws_vpc.prod-vpc.id
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = var.subnet-prefix[0].name
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  cidr_block = var.subnet-prefix[1].cidr_block
+  vpc_id = aws_vpc.prod-vpc.id
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = var.subnet-prefix[1].name
+  }
+}
+
+# 5. Associate Subnet with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow port 22, 80, 443
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Create a network interface with an IP in the subnet that was created in step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# 9. Create ubuntu server and install/enable apache2
+
+resource "aws_instance" "web-server-instance" {
+  ami = "ami-0bcc094591f354be2"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "main-key"
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = file("install_apache.sh")
+
+  tags = {
+    Name = "web-server"
+  }
+}
